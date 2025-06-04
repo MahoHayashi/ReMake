eb5f1e32af9097ba71f0427e663cf78b07bf3d89
MahoHayashi <is0792vf@ed.ritsumei.ac.jp>
2025-06-04 11:53:35 +0900

    Slack連携のテストコミット

--- ReMake/ContentView.swift
+++ ReMake/ContentView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack {Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+        }
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
