8407a5bfe64596d5f1668cd3fbc73e073c2aa52e
MahoHayashi <is0792vf@ed.ritsumei.ac.jp>
2025-06-04 11:48:50 +0900

    Initial Commit

---
+++ ReMake/ReMakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReMakeApp.swift
+//  ReMake
+//
+//  Created by maho hayashi on 2025/06/04.
+//
+
+import SwiftUI
+
+@main
+struct ReMakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReMakeTests/ReMakeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReMakeTests.swift
+//  ReMakeTests
+//
+//  Created by maho hayashi on 2025/06/04.
+//
+
+import Testing
+@testable import ReMake
+
+struct ReMakeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
