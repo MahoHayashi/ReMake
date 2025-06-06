8a8b9f24f82cd752c8ea361ea68ec415efe1085b
MahoHayashi <is0792vf@ed.ritsumei.ac.jp>
2025-06-06 19:41:18 +0900

    メイク一覧画面のプラスボタンが押されたらInputMakeupViewに遷移するようにNavigationLinkを作成

---
+++ ReMake/InputMakeupView.swift
@@ -0,0 +1,18 @@
+//
+//  InputMakeupView.swift
+//  ReMake
+//
+//  Created by maho hayashi on 2025/06/06.
+//
+
+import SwiftUI
+
+struct InputMakeupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputMakeupView()
+}
